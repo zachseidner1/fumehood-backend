3c63bccff1475d33f67c011f566b8f569e72cb37
zachseidner1 <zachary.seidner@gmail.com>
2023-05-05 19:13:36 -0400

    Added logger

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { DynamoDBClient, ListTablesCommand } from "@aws-sdk/client-dynamodb";
 import express from "express";
 import { config } from "./config/config";
 import { PORT } from "./config/config";
-
+import Logger from "./library/Logger";
 
 const router = express();
 
@@ -12,24 +12,31 @@ const client = new DynamoDBClient(config)
 
 /** Log requests */
 router.use((req, res, next) => {
-  console.log(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - 
-  IP: [${req.socket.remoteAddress}]`)
-
   res.on('finish', () => {
-    console.log(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: 
-    [${req.socket.remoteAddress}] - STATUS: ${res.statusCode}`)
+    if (res.statusCode >= 500) {
+      Logger.error(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: 
+    [${req.socket.remoteAddress}] - STATUS: ${Logger.colorStatusCode(res.statusCode)}`)
+    }
+    else {
+      Logger.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: ${Logger.colorStatusCode(res.statusCode)}`)
+    }
   })
   next();
 })
 
 /** Begin listening */
 router.listen(PORT, () => {
-  console.log(`Began listening on port ${PORT}`)
+  Logger.info(`Began listening on port ${PORT}`)
 })
 
 /** Base route - lists tables of the database */
 router.get("/", async (req, res) => {
   const lstTablesCommand = new ListTablesCommand({});
-  let response = await client.send(lstTablesCommand)
-  res.json(response.TableNames)
+  try {
+    let response = await client.send(lstTablesCommand)
+    res.json(response.TableNames)
+  } catch (ignored) {
+    res.status(500).json({ message: "Internal server error" })
+  }
+
 })
